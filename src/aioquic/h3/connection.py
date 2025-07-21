02ac53678b7a60ffdee3c77b18a7196cc21f75f8
Mahmoud Attia <attimoovy@gmail.com>
2025-07-21 13:26:37 +0300

    Allow duplicate setting identifiers

--- src/aioquic/h3/connection.py
+++ src/aioquic/h3/connection.py
@@ -187,8 +187,9 @@ def parse_settings(data: bytes) -> Dict[int, int]:
         value = buf.pull_uint_var()
         if setting in RESERVED_SETTINGS:
             raise SettingsError("Setting identifier 0x%x is reserved" % setting)
-        if setting in settings:
-            raise SettingsError("Setting identifier 0x%x is included twice" % setting)
+        # if setting in settings:
+        #     raise SettingsError("Setting identifier 0x%x is included twice" % setting)
+        # Allow duplicate setting identifiers for non-conformance
         settings[setting] = value
     return dict(settings)
 
